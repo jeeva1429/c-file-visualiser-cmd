a28289b57fc5637002542c24f6d072fa9b78945b
jeeva1429 <jparkkavan@gmail.com>
2025-04-21 14:08:09 +0530

    added formatting support after traveral

--- src/main.c
+++ src/main.c
@@ -14,37 +14,66 @@ typedef struct  directory_info {
 } dirInfo;
 
 //  read directory contents and returns info about the read directory
-void read_directory(char* file_name, dirInfo *dir) {
+void read_directory(char* file_name, dirInfo *dir, int directory_level) {
     DIR *dirp = opendir(file_name);
     if(dirp == NULL) {
         return;
     }
     struct dirent *dirent_struct;
-    char *filebuf = malloc(PATH_MAX * sizeof(char));  
+    char *filebuf = malloc(PATH_MAX);  
     if (filebuf == NULL) {
         printf("Memory allocation failed!\n");
         exit(1);  
     }
+
+    char *formatting_space = malloc(strlen("")+1 * sizeof(char));
+    strcpy(formatting_space, "");
+
     while((dirent_struct = readdir(dirp)) != NULL){
+        if (strcmp(dirent_struct->d_name, ".git") == 0) {
+            continue;
+        }        
         if (strcmp(dirent_struct->d_name, ".") == 0 || strcmp(dirent_struct->d_name, "..") == 0) {
             continue;  
         }        
         snprintf(filebuf, PATH_MAX, "%s/%s", file_name, dirent_struct->d_name);
-
         struct stat file_stat;
         stat(filebuf, &file_stat);  
+        char *formatting_space = malloc(sizeof(""));
+        strcpy(formatting_space,  "");
+        int start = 0;
+        int buffer_size = 1;
+        while (start < directory_level) {
+                int curr_len = strlen(formatting_space);
+                int extra_len = strlen("   ");
+                char *temp = realloc(formatting_space, curr_len + extra_len+ 1); 
+                if(temp != NULL){
+                    buffer_size = curr_len + extra_len + 1;
+                    formatting_space  = temp;
+                    snprintf(formatting_space + curr_len, buffer_size - strlen(formatting_space), "   ");
+                }
+                else {
+                    printf("Memory allocation failed!\n");
+                    exit(1);     
+                }
+                start++;
+            }
+        printf("%s|__ %s\n",formatting_space,dirent_struct->d_name);
         if(S_ISDIR(file_stat.st_mode)) {
-            printf("DIR:%s \n",dirent_struct->d_name);
             dir->dirCount++;
-            read_directory(filebuf, dir);
+            directory_level = directory_level + 1;
+            read_directory(filebuf, dir, directory_level);
         }
         else {
             dir->fileCount++;
-            printf("    FILE:%s \n",dirent_struct->d_name);
         }
-    }    
+        if(S_ISDIR(file_stat.st_mode)) {
+        directory_level--;
+        }
+    }   
     free(filebuf);  
     closedir(dirp); 
+    free(formatting_space);
 } 
 
 int main(int argc, char *argv[]) {  
@@ -68,7 +97,8 @@ int main(int argc, char *argv[]) {
     }
     // initial directory and files count
     dirInfo direct_info = {0,0};
-    read_directory(folder_name, &direct_info);
+    int directory_level = 0;
+    read_directory(folder_name, &direct_info, directory_level);
     printf("%d directories, %d files \n",direct_info.dirCount, direct_info.fileCount);
     free(folder_name);    
 
@@ -77,3 +107,8 @@ int main(int argc, char *argv[]) {
 }
 
 
+// [0,1,2,3,4]
+// 
+// 7
+// 1 2 1 2 1 2 1
+// -1 0 0 0 0 0 0
